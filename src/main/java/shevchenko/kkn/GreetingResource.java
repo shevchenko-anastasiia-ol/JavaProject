19b317488fa383061b35964ce7e64854bd0c48f8
shevchenko-anastasiia-ol <shevchenko.anastasiia.ol@gmail.com>
2025-09-16 13:54:57 +0300

    lab 2

--- src/main/java/shevchenko/kkn/GreetingResource.java
+++ src/main/java/shevchenko/kkn/GreetingResource.java
@@ -3,7 +3,11 @@ package shevchenko.kkn;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
+
+import java.security.Principal;
 
 @Path("/hello")
 public class GreetingResource {
@@ -13,4 +17,20 @@ public class GreetingResource {
     public String hello() {
         return "Hello from Quarkus REST";
     }
+
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String whoAmI(@Context SecurityContext securityContext) {
+        Principal userPrincipal = securityContext.getUserPrincipal();
+        if (userPrincipal != null) {
+            return userPrincipal.getName();
+        } else {
+            return "anonymous";
+        }
+    }
 }
+
+
+
