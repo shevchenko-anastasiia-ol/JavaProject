f2b6f6116d72f63c1dcde1979ad6c767bb11b844
shevchenko-anastasiia-ol <shevchenko.anastasiia.ol@gmail.com>
2025-09-16 13:00:25 +0300

    Initial commit

---
+++ src/test/java/shevchenko/kkn/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package shevchenko.kkn;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
